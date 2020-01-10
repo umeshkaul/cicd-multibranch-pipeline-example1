1244e73f5c543d8a9f3e77a6c152b019ca678afe
umeshkaul <umeshkaul@gmail.com>
2020-01-09 20:11:34 -0500

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -7,17 +7,17 @@ class TestFunctions(unittest.TestCase):
     """ Test add_values
     """
     def setUp(self):
-        self.value1 = 10
+        self.value1 = 20
         self.value2 = 20
 
     def test_add_values(self):
-        self.assertEqual(30, add_two_values(self.value1, self.value2))
+        self.assertEqual(40, add_two_values(self.value1, self.value2))
 
     def test_multiply_two_values(self):
-        self.assertEqual(200,multipy_two_values(self.value1,self.value2))
+        self.assertEqual(400,multipy_two_values(self.value1,self.value2))
 
     def test_add_values_two(self):
-        self.assertEqual(15, add_multiple_values(1,2,3,4,5))
+        self.assertEqual(16, add_multiple_values(1,1,2,3,4,5))
 
     def test_full_name(self):
         self.assertEqual("Doe, John", get_full_name("John", "Doe"))
